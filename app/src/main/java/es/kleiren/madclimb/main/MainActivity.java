a8cdd133b970ca9d5d9a51d6df595235c89ea81a
Carlos Sanchez Redondo <carlos.sanchezredondo@bq.com>
2019-07-09 15:28:23 +0200

    -v 0.33

--- app/src/main/java/es/kleiren/madclimb/main/MainActivity.java
+++ app/src/main/java/es/kleiren/madclimb/main/MainActivity.java
@@ -1,11 +1,9 @@
 package es.kleiren.madclimb.main;
 
-import android.Manifest;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,19 +11,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -37,7 +31,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.LibsBuilder;
 
 import butterknife.BindView;
@@ -58,10 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     BottomNavigationView bottomNavigationView;
     private DatabaseReference mDatabase;
     public String updates;
-    private boolean shownNewZones = false;
-
-    private SwitchCompat switcher;
-
 
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
